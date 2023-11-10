43bd027acfc4d488c6f176fc08b432d0c321adca
Jerome Dumonteil <jd@abilian.com>
2023-11-10 11:57:05 +0100

    fix: lock conversion handlers to instance var/run to fix circular import

--- src/abilian/services/conversion/handler_lock.py
+++ src/abilian/services/conversion/handler_lock.py
@@ -1,21 +1,19 @@
 import functools
+import os
 from collections.abc import Callable
 from pathlib import Path
 
 from filelock import FileLock, Timeout
 
 from .exceptions import ConversionError
-from .util import get_tmp_dir
 
 LOCK_EXPIRE = 1800  # 30 min, in case many request in //
 
 
 def acquire_lock(name: str) -> Callable:
     """Ensure the decorated function is alone to run by using a lock file."""
-    lock = FileLock(
-        get_tmp_dir() / f"{name}.lock",
-        timeout=LOCK_EXPIRE,
-    )
+    var_run = Path(os.environ.get("INSTANCE_VAR_RUN", "/var/run"))
+    lock = FileLock(var_run / f"{name}.lock", timeout=LOCK_EXPIRE)
 
     def locked(func):
         @functools.wraps(func)
